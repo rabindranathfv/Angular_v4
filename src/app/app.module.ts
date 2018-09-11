72104a221e5b22411061a9105efd8c15f6650c49
Rabindranath <ferreiralox@gmail.com>
2018-09-11 00:05:16 -0300

    add portfolio item component and using it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortfolioComponent } from './pagues/portfolio/portfolio.component';
+import { AboutComponent } from './pagues/about/about.component';
+import { PortfolioItemComponent } from './pagues/portfolio-item/portfolio-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    AboutComponent,
+    PortfolioItemComponent
   ],
   imports: [
     BrowserModule
