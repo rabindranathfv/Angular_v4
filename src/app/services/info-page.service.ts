460c60e650865f58168e5a4a7ad8db446a83f678
Rabindranath <ferreiralox@gmail.com>
2018-09-14 12:38:23 -0300

    using dinamyc data with firebase in a main service

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -10,18 +10,32 @@ export class InfoPageService {
   // mala practica usar any, se declaran interfaces dentro de la app para q conozca los elementos que se usaran en el servicio
   info: infoPageInterface = {};
   load = false;
+  team_load = false;
+  team: any = [];
 
   constructor( private http: HttpClient ) {
 
     // console.log("using infoPage Service");
+    this.loadInfo();
+    this.loadTeam();
 
-    // get data from json
-    this.http.get('assets/data/data-page.json').subscribe( ( resp: infoPageInterface ) => {
+  }
+
+  private loadInfo() {
+// get data from json
+    this.http.get('https://angularv6portfolio.firebaseio.com/equipo.json').subscribe( ( resp: infoPageInterface ) => {
       this.info = resp;
 
       this.load = true;
       console.log(resp);
     });
+  }
 
+  private loadTeam() {
+    this.http.get('https://angularv6portfolio.firebaseio.com/equipo.json').subscribe( ( resp: any[] ) => {
+      this.team = resp;
+      this.team_load = true;
+      console.log(resp);
+    });
   }
 }
