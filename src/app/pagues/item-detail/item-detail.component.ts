26d675111322989196565de7e34fee22307c47de
Rabindranath <ferreiralox@gmail.com>
2018-09-17 20:21:46 -0300

    dynamic data in item-detail views using the component

--- src/app/pagues/item-detail/item-detail.component.ts
+++ src/app/pagues/item-detail/item-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../../services/products.service';
+import { detailProducts } from '../../interfaces/detailProducts.interface';
 
 @Component({
   selector: 'app-item-detail',
@@ -9,16 +10,21 @@ import { ProductsService } from '../../services/products.service';
 })
 export class ItemDetailComponent implements OnInit {
 
+  prodctDetail: detailProducts;
+  idP: string;
+
   constructor( private route: ActivatedRoute,
                public productsService: ProductsService ) { }
 
   ngOnInit() {
     // para tomar los parametros de las interfaces de usuario
     this.route.params.subscribe( parameters => {
-      console.log(parameters);
+      // console.log(parameters);
 
-      this.productsService.getProducts(parameters.id)
-          .subscribe( product => {
+      this.productsService.getProducts(parameters['id'])
+          .subscribe( ( product: detailProducts ) => {
+            this.idP = parameters['id'];
+            this.prodctDetail = product;
             console.log(product);
           });
       });
