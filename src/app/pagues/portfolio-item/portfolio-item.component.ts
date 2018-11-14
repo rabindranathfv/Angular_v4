71a9175482d2168a2b42d9f15bb603b5a419a3b9
Unknown <ferreiralox@gmail.com>
2018-11-14 04:19:31 -0300

    using ngFor with ngIf with even and odd index

--- src/app/pagues/portfolio-item/portfolio-item.component.ts
+++ src/app/pagues/portfolio-item/portfolio-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-portfolio-item',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioItemComponent implements OnInit {
 
-  constructor() { }
+  constructor( public infoProductsInterface: ProductsService) { }
 
   ngOnInit() {
   }
